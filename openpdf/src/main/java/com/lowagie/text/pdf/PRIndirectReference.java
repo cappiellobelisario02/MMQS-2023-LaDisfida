249ad86b2022c63a6241a240c63b79cbb6152ca1
GiovanniZagaria <116624829+GiovanniZagaria@users.noreply.github.com>
2024-06-11 17:08:18 +0200

    c

--- openpdf/src/main/java/com/lowagie/text/pdf/PRIndirectReference.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/PRIndirectReference.java
@@ -86,7 +86,7 @@ public class PRIndirectReference extends PdfIndirectReference {
     }
 
     // methods
-
+    @Override
     public void toPdf(PdfWriter writer, OutputStream os) throws IOException {
         int n = writer.getNewObjectNumber(reader, number, generation);
         os.write(PdfEncodings.convertToBytes(new StringBuffer().append(n).append(" 0 R").toString(), null));
