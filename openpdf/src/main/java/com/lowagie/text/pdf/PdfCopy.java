e345e49e9b9a58df6cefc025f961fa1a708efe06
SabboC <144833578+SabboC@users.noreply.github.com>
2024-07-31 19:38:51 +0200

    31072024

--- openpdf/src/main/java/com/lowagie/text/pdf/PdfCopy.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/PdfCopy.java
@@ -790,7 +790,7 @@ public class PdfCopy extends PdfWriter {
                             PdfRectangle rect = (PdfRectangle) annot.get(PdfName.RECT);
                             if (rect != null && (rect.left() != 0 || rect.right() != 0 || rect.top() != 0
                                     || rect.bottom() != 0)) {
-                                int rotation = reader.getPageRotation(pageN);
+                                int rotation = PdfReader.getPageRotation(pageN);
                                 Rectangle pageSize = reader.getPageSizeWithRotation(pageN);
                                 switch (rotation) {
                                     case 90:
