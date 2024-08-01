e67af696a4138e9af009cdf9e9cc1176df921c93
SabboC <144833578+SabboC@users.noreply.github.com>
2024-08-01 12:57:30 +0200

    01082024

--- openpdf/src/main/java/com/lowagie/text/pdf/PdfDocument.java
+++ openpdf/src/main/java/com/lowagie/text/pdf/PdfDocument.java
@@ -1053,6 +1053,7 @@ public class PdfDocument extends Document {
      * @see com.lowagie.text.DocListener#setMarginMirroring(boolean)
      * @since 2.1.6
      */
+    @Override
     public boolean setMarginMirroringTopBottom(boolean MarginMirroringTopBottom) {
         if (writer != null && writer.isPaused()) {
             return false;
