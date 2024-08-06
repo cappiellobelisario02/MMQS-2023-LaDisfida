10e96284e77e9c0aa83dea9b8faa5442eb1e62fb
SabboC <144833578+SabboC@users.noreply.github.com>
2024-08-06 18:32:48 +0200

    06082024

---
+++ openpdf/src/main/java/com/lowagie/text/exceptions/IllegalReferencePointerException.java
@@ -0,0 +1,9 @@
+package com.lowagie.text.exceptions;
+
+import java.lang.RuntimeException;
+
+public class IllegalReferencePointerException extends RuntimeException {
+    public IllegalReferencePointerException(String message) {
+        super(message);
+    }
+}
